7247c6ea98b74e519cd984ffe1b6c3b7b4fa5b8f
DANILO <DANILO@DANILO>
2017-04-25 21:32:11 -0300

    Merge origin/master
    Conflicts:
    	src/Telas/Login.java

---
+++ src/Telas/Principal.java
@@ -0,0 +1,17 @@
+package Telas;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author DANILO
+ */
+public class Principal extends Application  {
+
+    @Override
+    public void start(Stage primaryStage)  {
+        
+    }
+    
+}
