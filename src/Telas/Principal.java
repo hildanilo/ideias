0fef3c9a9716101758af08ace22e0417aba96d8b
DANILO <DANILO@DANILO>
2017-04-25 21:54:53 -0300

    Merge origin/master
    Conflicts:
    	src/Telas/Login.java

--- src/Telas/Principal.java
+++ src/Telas/Principal.java
@@ -1,6 +1,8 @@
 package Telas;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 /**
@@ -11,7 +13,25 @@ public class Principal extends Application  {
 
     @Override
     public void start(Stage primaryStage)  {
+       
         
+        StackPane root = new StackPane();
+        root.getChildren().addAll();
+        
+        Scene scene = new Scene(root, 1800, 800);
+        
+        primaryStage.getMinHeight();
+        primaryStage.setMinHeight(10);
+        primaryStage.setTitle("Principal");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
     }
     
 }
